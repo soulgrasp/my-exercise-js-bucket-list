d4d3306fc289f4ace7894948be3ce797c4003d23
Niklas Bengtsson <de02beni@gmail.com>
2024-08-19 06:33:28 +0200

    Bootstrapping the solution, use bucketlist as sample project clean later

---
+++ domMan.js
@@ -0,0 +1,42 @@
+
+
+// get element by id
+const listDesc = document.getElementById('section-list');
+
+console.log(listDesc);
+
+// getElementByClassname
+const pTags = document.getElementsByClassName('content');
+console.log(pTags);
+
+
+// getElementByName  // searches the name attribut (input elements)
+// getElementsByTagname
+
+// querySelector  Search element by the css syntax
+// takes a string 
+// is applicable on HTML as well as the document object
+const item = document.querySelector('.item'); // CSS syntax selector, same as stylesheet
+console.log(item);
+
+const mainSection = document.querySelector("#main-section");
+console.log(mainSection);
+
+// alternative 1  searching from document top
+const nestedItem = document.querySelector("#main-section .item"); // Id selector CSS style
+// gets the span element hits section on ID and the class selector "item" get the span element
+console.log(nestedItem);
+
+// alternative way search from parent element
+const nestedItem2 = mainSection.querySelector(".item");
+console.log(nestedItem2);
+
+// 
+
+
+
+// 
+
+
+
+
